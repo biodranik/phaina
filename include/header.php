5327e89e7fed3f5d63f7fc52dc0d2967afc1686e
Alex Zolotarev <me@alex.bio>
2017-01-29 16:22:39 +0300

    Minor formatting

--- include/header.php
+++ include/header.php
@@ -3,16 +3,16 @@
   <nav class="nav">
     <ul class="menu">
       <?php foreach (MainMenu() as $m) : ?>
-        <li class="menu__item">
-          <a class="menu__link<?php if ($m->isCurrent) echo ' menu__link--selected'?>" href="<?= $m->url ?>"><?= $m->title ?></a>
-        </li>
-        <?php endforeach; ?>
-        <li class="menu__item menu__item--login">
-          <a class="menu__link menu__link--login" href="https://www.dev.vibrobox.com/site/login">
-            <i class="fa fa-sign-in"></i>
-            <?php T("loginButton"); ?>
-          </a>
-        </li>
+      <li class="menu__item">
+        <a class="menu__link<?php if ($m->isCurrent) echo ' menu__link--selected'?>" href="<?= $m->url ?>"><?= $m->title ?></a>
+      </li>
+      <?php endforeach; ?>
+      <li class="menu__item menu__item--login">
+        <a class="menu__link menu__link--login" href="https://www.dev.vibrobox.com/site/login">
+          <i class="fa fa-sign-in"></i>
+          <?php T("loginButton"); ?>
+        </a>
+      </li>
     </ul>
   </nav>
 </header>
