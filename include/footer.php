c911e9b1bff47f2aee76225cdba4e8a4cf655883
Aliaksei Maniuk <aliaksei.maniuk@gmail.com>
2017-01-29 18:03:08 -0500

    Simple footer implementation.

--- include/footer.php
+++ include/footer.php
@@ -1,3 +1,6 @@
 <footer>
-  <p>&copy; 2017 VibroBox OÜ</p>
+  <p class="footer__copyright footer__copyright--separated">&copy; 2017 VibroBox OÜ</p>
+  <?php foreach (MainMenu() as $m) : ?>
+    <a class="footer__link" href="<?= $m->url ?>"><?= $m->title ?></a>
+  <?php endforeach; ?>
 </footer>
