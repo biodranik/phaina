9fdc92fca5bbbb3f0677f72f67525e0593511a88
Alex Zolotarev <me@alex.bio>
2017-01-29 15:42:56 +0300

    URL() and BaseURL() functions.

--- include/head.php
+++ include/head.php
@@ -1,12 +1,12 @@
-<base href="<?php echo BASE_URL; ?>">
+<base href="<?= BaseURL() ?>">
 
 <?php require_once("meta.php"); ?>
 
 <!-- TODO create CDN link to FontAwesome from vb account -->
 <script src="https://use.fontawesome.com/998e59ad4f.js"></script>
 
-<link rel="icon" type="image/x-icon" href="favicon.ico?">
-<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="icon" type="image/x-icon" href="<?= URL('favicon.ico') ?>?">
+<link rel="stylesheet" type="text/css" href="<?= URL('css/style.css') ?>">
 
 <!-- TODO: Generate hreflang links for every language.
   <link href="" rel="alternate" hreflang="">
