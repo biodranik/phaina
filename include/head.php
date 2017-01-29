263881b6a1dffd08640a8eadb84bd521bce7c88e
Aliaksei Maniuk <aliaksei.maniuk@gmail.com>
2017-01-29 16:50:25 -0500

    Removed fontawesome.

--- include/head.php
+++ include/head.php
@@ -5,9 +5,6 @@
   <base href="<?= BaseURL() ?>">
   <?php require_once("meta.php"); ?>
 
-  <!-- TODO create CDN link to FontAwesome from vb account -->
-  <script src="https://use.fontawesome.com/998e59ad4f.js"></script>
-
   <link rel="icon" type="image/x-icon" href="<?= URL('favicon.ico') ?>?">
   <link rel="stylesheet" type="text/css" href="<?= URL('css/style.css') ?>">
 
--- include/header.php
+++ include/header.php
@@ -8,10 +8,8 @@
       </li>
       <?php endforeach; ?>
       <li class="menu__item menu__item--login">
-        <a class="menu__link menu__link--login" href="https://www.dev.vibrobox.com/site/login">
-          <i class="fa fa-sign-in"></i><?= T("loginButton") ?></a>
+        <a class="menu__link menu__link--login" href="https://www.dev.vibrobox.com/site/login"><?= T("loginButton") ?></a>
       </li>
     </ul>
   </nav>
 </header>
-
