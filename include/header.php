4c11dc43b5a1ea40f42247d644e61e46cc1721ef
Aliaksei Maniuk <aliaksei.maniuk@gmail.com>
2017-01-29 17:29:50 -0500

    Login button on-hover behaviour. Ignored debug.log.

--- include/header.php
+++ include/header.php
@@ -1,15 +1,13 @@
 <header class="header">
   <a href="<?= BaseURL() ?>" class="logo"></a>
-  <nav class="nav">
-    <ul class="menu">
-      <?php foreach (MainMenu() as $m) : ?>
-      <li class="menu__item">
-        <a class="menu__link<?php if ($m->isCurrent) echo ' menu__link--selected'?>" href="<?= $m->url ?>"><?= $m->title ?></a>
-      </li>
-      <?php endforeach; ?>
-      <li class="menu__item menu__item--login">
-        <a class="menu__link menu__link--login" href="https://www.dev.vibrobox.com/site/login"><?= T("loginButton") ?></a>
-      </li>
-    </ul>
-  </nav>
+  <ul class="menu">
+    <?php foreach (MainMenu() as $m) : ?>
+    <li class="menu__item">
+      <a class="menu__link<?php if ($m->isCurrent) echo ' menu__link--selected'?>" href="<?= $m->url ?>"><?= $m->title ?></a>
+    </li>
+    <?php endforeach; ?>
+    <li class="menu__item menu__item--login">
+      <a class="menu__link menu__link--login" href="https://www.dev.vibrobox.com/site/login"><?= T("loginButton") ?></a>
+    </li>
+  </ul>
 </header>
