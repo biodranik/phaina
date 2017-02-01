6c0ef4b0915e22d06f931d06d5d393f9108839e6
DESKTOP-IQQFBGN\misha <mikhail.maniuk@gmail.com>
2017-02-02 00:28:16 +0300

    change footer design

--- include/footer.php
+++ include/footer.php
@@ -1,12 +1,12 @@
 <footer class="footer">
   <div class="footer__container">
-    <ul class="footer__nav">
+    <ul class="footer-nav">
       <?php foreach (MainMenu() as $m) : ?>
-      <li class="footer__nav-item">
-        <a class="footer__link" href="<?= $m->url ?>"><?= $m->title ?></a>
-      </li>
+        <li class="footer-nav__item">
+          <a class="footer__link" href="<?= $m->url ?>"><?= $m->title ?></a>
+        </li>
       <?php endforeach; ?>
     </ul>
-    <p class="footer__copyright">© 2017 VibroBox OÜ</p>
+    <p class="footer__copyright">&copy; 2017 VibroBox OÜ</p>
   </div>
 </footer>
