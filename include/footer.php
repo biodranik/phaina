47077e31409a829b2a6d3bb9ef2f80c473e05dbe
Alex Zolotarev <me@alex.bio>
2017-02-02 22:29:07 +0300

    New footer from pull request.

--- include/footer.php
+++ include/footer.php
@@ -1,6 +1,12 @@
-<footer>
-  <p class="footer__copyright footer__copyright--separated">&copy; 2017 VibroBox OÜ</p>
-  <?php foreach (MainMenu() as $m) : ?>
-    <a class="footer__link" href="<?= $m->url ?>"><?= $m->title ?></a>
-  <?php endforeach; ?>
+<footer class="footer">
+  <div class="footer__container">
+    <ul class="footer-nav">
+      <?php foreach (MainMenu() as $m) : ?>
+        <li class="footer-nav__item">
+          <a class="footer__link" href="<?= $m->url ?>"><?= $m->title ?></a>
+        </li>
+      <?php endforeach; ?>
+    </ul>
+    <p class="footer__copyright">&copy; 2017 VibroBox OÜ</p>
+  </div>
 </footer>
