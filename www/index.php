5304732e15c8d52bd5d84b3d7cfac5136489ad45
Aliaksei Maniuk <aliaksei.maniuk@gmail.com>
2017-02-03 22:01:40 -0500

    Moved index page models initialization from globals.php

--- www/index.php
+++ www/index.php
@@ -9,7 +9,91 @@
   // TODO: Probably it can be moved to the config.php.
   require_once(dirname(__FILE__).'/../config.php');
   require(dirname(__FILE__).'/../include/uri_routing.php');
+
   HTML_HEAD();
+?>
+
+<?php 
+  // Initialization of page data models.
+
+  $plusSectionItems = [
+    'plusAutomaticSystem' => [
+      'title' => T('plusAutomaticSystemTitle'), 
+      'description' => T('plusAutomaticSystemDescription'),
+      'icon' => 'plus-icon__automatic'
+    ],
+    'plusEconomy' => [
+      'title' => T('plusEconomyTitle'), 
+      'description' => T('plusEconomyDescription'),
+      'icon' => 'plus-icon__economy'
+    ],
+    'plusAvailability' => [
+      'title' => T('plusAvailabilityTitle'), 
+      'description' => T('plusAvailabilityDescription'),
+      'icon' => 'plus-icon__availability'
+    ],
+    'plusSimpliticy' => [
+      'title' => T('plusSimpliticyTitle'), 
+      'description' => T('plusSimpliticyDescription'),
+      'icon' => 'plus-icon__simpliticy'
+    ]
+  ];
+
+  $solutionSectionItems = [
+    'systemEquipment' => [
+      'title' => T('systemEquipmentTitle'), 
+      'description' => T('systemEquipmentDescription'), 
+      'css' => 'system-container__equipment',
+      'icon' => 'system-icon__equipment'
+    ],
+    'systemTransfer' => [
+      'title' => T('systemTransferTitle'), 
+      'description' => T('systemTransferDescription'),
+      'css' => 'system-container__transfer',
+      'icon' => 'system-icon__transfer'
+    ],
+    'systemProcessing' => [
+      'title' => T('systemProcessingTitle'), 
+      'description' => T('systemProcessingDescription'),
+      'css' => 'system-container__processing',
+      'icon' => 'system-icon__processing'
+    ],
+    'systemGReport' => [
+      'title' => T('systemGReportTitle'), 
+      'description' => T('systemGReportDescription'),
+      'css' => 'system-container__g-report',
+      'icon' => 'system-icon__g-report'
+    ],
+    'systemUReport' => [
+      'title' => T('systemUReportTitle'), 
+      'description' => T('systemUReportDescription'), 
+      'css' => 'system-container__u-report',
+      'icon' => 'system-icon__u-report'
+    ]
+  ];
+
+  $faqSectionItems = [
+    'q1' => [
+      'question' => T("faqQuestion1"),
+      'title' => T("faqTitle1"),
+      'answer' => T("faqAnswer1")
+    ],
+    'q2' => [
+      'question' => T("faqQuestion2"),
+      'title' => T("faqTitle2"),
+      'answer' => T("faqAnswer2")
+    ],
+    'q3' => [
+      'question' => T("faqQuestion3"),
+      'title' => T("faqTitle3"),
+      'answer' => T("faqAnswer3")
+    ],
+    'q4' => [
+      'question' => T("faqQuestion4"),
+      'title' => T("faqTitle4"),
+      'answer' => T("faqAnswer4")
+    ]
+  ]
 ?>
 
 <body>
@@ -18,7 +102,7 @@
 
 <main role="main">
   
-  <section class="section banner" id="banner">
+  <section class="section banner">
     <div class="section__container section__container--banner">
       <h1 class="title title__main">
         <?= T("indexMainTitle") ?>
@@ -30,11 +114,11 @@
     </div>
   </section>
 
-  <section class="section plus" id="benefits">
+  <section class="section plus">
     <div class="section__container">
       <h2 class="title title__second"><?= T("plusTitle") ?></h2>
       <div class="plus-container">
-        <?php foreach (GetPlusSectionItems() as $item) : ?>
+        <?php foreach ($plusSectionItems as $item) : ?>
           <div class="plus-container__item">
             <h3 class="plus-container__title plus-icon <?php echo $item['icon'] ?>">
               <span><?= $item['title'] ?></span>
@@ -46,14 +130,14 @@
     </div>
   </section>
 
-  <section class="section system" id="solution">
+  <section class="section system">
     <div class="section__container">
       <h2 class="title title__second"><?= T("systemItem") ?></h2>
       <p class="preface">
         <?= T("systemPreface") ?>
       </p>
       <div class="system-container">
-        <?php foreach (GetSolutionSectionItems() as $item) : ?>
+        <?php foreach ($solutionSectionItems as $item) : ?>
           <div class="system-container__item  <?php echo $item['css'] ?> ">
             <h3 class="system-container__title system-icon <?php echo $item['icon'] ?>">
               <?= $item['title'] ?>
@@ -64,29 +148,13 @@
       </div>
 
       <div class="faq">
-        <div class="faq__item">
-          <div class="faq__question"><?= T("faqQuestion1") ?></div>
-          <div class="faq__title"><?= T("faqTitle1") ?></div>
-          <p class="faq__answer"><?= T("faqAnswer1") ?></p>
-        </div>
-
-        <div class="faq__item">
-          <div class="faq__question"><?= T("faqQuestion2") ?></div>
-          <div class="faq__title"><?= T("faqTitle2") ?></div>
-          <p class="faq__answer"><?= T("faqAnswer2") ?></p>
-        </div>
-
-        <div class="faq__item">
-          <div class="faq__question"><?= T("faqQuestion3") ?></div>
-          <div class="faq__title"><?= T("faqTitle3") ?></div>
-          <p class="faq__answer"><?= T("faqAnswer3") ?></p>
-        </div>
-
-        <div class="faq__item">
-          <div class="faq__question"><?= T("faqQuestion4") ?></div>
-          <div class="faq__title"><?= T("faqTitle4") ?></div>
-          <p class="faq__answer"><?= T("faqAnswer4") ?></p>
-        </div>
+        <?php foreach($faqSectionItems as $item) : ?>
+          <div class="faq__item">
+            <div class="faq__question"><?= $item['question'] ?></div>
+            <div class="faq__title"><?= $item['title'] ?></div>
+            <p class="faq__answer"><?= $item['answer'] ?></p>
+          </div>
+        <?php endforeach; ?>
       </div>
 
     </div>
