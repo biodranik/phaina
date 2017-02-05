eec5fd7743d623561c4b8c35eaa999452f2bbfa6
Alexander Zolotarev <me@alex.bio>
2017-02-05 16:41:50 +0300

    Restyled flexbox footer.

--- include/footer.php
+++ include/footer.php
@@ -1,12 +1,13 @@
 <footer class="footer">
-  <div class="footer__container">
-    <ul class="footer__nav">
-      <?php foreach (MainMenu() as $m) : ?>
-      <li class="footer__nav-item">
-        <a class="footer__link" href="<?= $m->url ?>"><?= $m->title ?></a>
-      </li>
-      <?php endforeach; ?>
-    </ul>
-    <p class="footer__copyright">© 2017 VibroBox OÜ</p>
-  </div>
+  <ul class="footer__nav">
+    <?php foreach (MainMenu() as $m) : ?>
+    <li class="footer__nav-item">
+      <a class="footer__link" href="<?= $m->url ?>"><?= $m->title ?></a>
+    </li>
+    <?php endforeach; ?>
+    <li class="footer__nav-item footer__nav-item--login">
+      <a class="footer__link footer__link--login" href="<?= DEMO_URL ?>"><?= T('loginButton') ?></a>
+    </li>
+  </ul>
+  <p class="footer__copyright">© 2017 VibroBox OÜ</p>
 </footer>
