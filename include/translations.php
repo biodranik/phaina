663c38becf8ea080ee7ddb15b599d7a62781c15d
Alexander Zolotarev <me@alex.bio>
2017-02-12 01:52:42 +0300

    $translations => $TRANSLATIONS.

--- include/translations.php
+++ include/translations.php
@@ -10,24 +10,24 @@
 //   }
 // }
 
-// Load translations from external file to global variable.
-$translations = json_decode(file_get_contents(dirname(__FILE__)."/../translations/strings.json"), true);
-if ($translations === NULL || json_last_error() != JSON_ERROR_NONE) exit(json_last_error_msg() . ": strings.json can not be loaded.");
+// Load all strings to the global variable $TRANSLATIONS.
+$TRANSLATIONS = json_decode(file_get_contents(dirname(__FILE__)."/../translations/strings.json"), true);
+if ($TRANSLATIONS === NULL || json_last_error() != JSON_ERROR_NONE) exit(json_last_error_msg() . ": strings.json can not be loaded.");
 
 // Returns translated string if translation is present, otherwise
 // returns translation in default language if translation is absent, otherwise
 // returns the key itself.
 function T($key, $lang = LANG) {
-  global $translations;
+  global $TRANSLATIONS;
 
   // Bad: given key is not translated at all. Use it as a translation.
-  if (!array_key_exists($key, $translations)) return $key;
+  if (!array_key_exists($key, $TRANSLATIONS)) return $key;
   // Good: we have a translation for given language.
-  if (array_key_exists($lang, $translations[$key])) return $translations[$key][$lang];
+  if (array_key_exists($lang, $TRANSLATIONS[$key])) return $TRANSLATIONS[$key][$lang];
   // Bad: default language translation is missing. Key is used by default.
   if ($lang == $defaultLanguage) return $key;
   // Not good: translation is missing but at least we have a default one.
-  if (array_key_exists($defaultLanguage, $translations[$key])) return $translations[$key][$defaultLanguage];
+  if (array_key_exists($defaultLanguage, $TRANSLATIONS[$key])) return $TRANSLATIONS[$key][$defaultLanguage];
   // Bad: both target and default language translations are missing. Key is used by default.
   return $key;
 }
