627660ffacff356ef812e4a61e2d4ac086f8dc93
Aliaksei Maniuk <aliaksei.maniuk@gmail.com>
2017-02-14 23:07:36 -0500

    fixed anchors

--- www/faq.php
+++ www/faq.php
@@ -20,33 +20,8 @@ $sections =[[
   [
     'question' => T("general_q4_question"),
     'anchor' => T('general_q4_anchor'),
-    'detailedAnswer' => T("general_q4_answer")]]],
-[
-  'sectionName' => T("generalQuestionsTitle"),
-  'anchor' => 'genetal-questions1',
-  'questions' => [[
-    'question' => T("general_q1_question"),
-    'anchor' => 'q11',
-    'detailedAnswer' => T("general_q1_answer")],
-  [
-    'question' => T("general_q2_question"),
-    'anchor' => 'q21',
-    'detailedAnswer' => T("general_q2_answer")], 
-  [
-    'question' => T("general_q3_question"),
-    'anchor' => 'q31',
-    'detailedAnswer' => T("general_q3_answer")],
-  [
-    'question' => T("general_q4_question"),
-    'anchor' => 'q41',
     'detailedAnswer' => T("general_q4_answer")]]]
 ];
-
-$pageFullPath = GetCurrentPageFullPath();
-
-function BuildUrlWithAnchor($pageFullPath, $anchor) {
-  return $pageFullPath . '#' . $anchor;  
-}
 ?>
 
 <body>
@@ -57,24 +32,23 @@ function BuildUrlWithAnchor($pageFullPath, $anchor) {
   <section class="section">
     <h1><?= T("faqTitle") ?></h1>
     <p class="preface"><?= T("faqPreface") ?></p>
-
     <?php foreach ($sections as $s) : ?>
-      <div class="faq-summary-section">
-        <h2 class="faq-summary-section__h2">
-          <a href="<?= BuildUrlWithAnchor($pageFullPath, $s['anchor']) ?>">
-            <?= $s['sectionName'] ?>
+    <div class="faq-summary-section">
+      <h2 class="faq-summary-section__h2">
+        <a href="#<?= $s['anchor'] ?>">
+          <?= $s['sectionName'] ?>
+        </a>
+      </h2>
+      <ul class="faq-summary-section__list">
+      <?php foreach ($s['questions'] as $q) : ?>
+        <li class="faq-summary-section__question">
+          <a href="#<?= $q['anchor'] ?>">
+            <?= $q['question'] ?>
           </a>
-        </h2>
-        <ul class="faq-summary-section__list">
-          <?php foreach ($s['questions'] as $q) : ?>
-            <li class="faq-summary-section__question">
-              <a href="<?= BuildUrlWithAnchor($pageFullPath, $q['anchor']) ?>">
-                <?= $q['question'] ?>
-              </a>
-            </li>
-          <?php endforeach; ?>
-        </ul>
-      </div>
+        </li>
+      <?php endforeach; ?>
+      </ul>
+    </div>
     <?php endforeach; ?>
   </section>
 
