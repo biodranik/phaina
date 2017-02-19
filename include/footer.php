e1278bb58f79aa43dbc0f98f96b04ea5388ecab6
Alexander Zolotarev <me@alex.bio>
2017-02-19 16:31:10 +0300

    Google Analytics.

--- include/footer.php
+++ include/footer.php
@@ -11,3 +11,21 @@
   </ul>
   <p class="footer__copyright">© 2017 VibroBox OÜ</p>
 </footer>
+
+<?php // Do not load Google Analytics when developing on a localhost machine.
+if (!IsLocalhostDevelopmentMode()): ?>
+<script>
+(function(i, s, o, g, r, a, m) {
+  i['GoogleAnalyticsObject'] = r;
+  i[r] = i[r] || function() {
+    (i[r].q = i[r].q || []).push(arguments)
+  }, i[r].l = 1 * new Date();
+  a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+ga('create', '<?= GOOGLE_ANALYTICS_ID ?>', 'auto');
+ga('send', 'pageview');
+</script>
+<?php endif ?>
