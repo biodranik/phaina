a4101136968fe23bc33f6947b7e3eb067ea68125
Alexander Zolotarev <me@alex.bio>
2017-02-19 23:13:15 +0300

    Stub for hreflang.

--- include/head.php
+++ include/head.php
@@ -7,9 +7,17 @@
   <link rel="icon" type="image/x-icon" href="<?= URL('favicon.ico') ?>?">
   <link rel="stylesheet" type="text/css" href="<?= URL('css/style.css') ?>">
 
-  <!-- TODO: Generate hreflang links for every language.
-    <link href="" rel="alternate" hreflang="">
-  -->
+  <!-- TODO: Generate hreflang links for every language. -->
+  <?php /* $langSites = [
+      'https://www.vibrobox.com/' => ['x-default', 'en'],
+      'https://www.vibrobox.ru/' => ['ru', 'uk', 'kk'],
+      'https://www.vibrobox.by/' => ['be'],
+    ];
+  ?>
+  <?php foreach ($langSites as $url => $langs) : foreach ($langs as $lang) : ?>
+  <link href="<?= $url ?>" hreflang="<?= $lang ?>" rel="alternate">
+  <?php endforeach; endforeach; */?>
+
   <?php
     foreach ($HEAD_TAGS as $tag) echo "$tag\n";
   ?>
