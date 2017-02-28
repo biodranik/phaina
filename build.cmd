2936a798da7dc088d819d7db010c6433783ebdd1
Aliaksei Maniuk <aliaksei.maniuk@gmail.com>
2017-02-27 21:05:43 -0500

    Optimized loading of fonts.

--- build.cmd
+++ build.cmd
@@ -13,7 +13,7 @@ for %%f in (bin\sassc*%PROCESSOR_ARCHITECTURE%.exe) do SET sassc=%%f
 SET sassc_input=scss/style.scss
 SET sassc_output=www/css/style.css
 REM One of: nested, expanded, compact, compressed
-SET sassc_style=nested
+SET sassc_style=compressed
 
 %sassc% --style %sassc_style% %sassc_input% %sassc_output% || ECHO ERROR while launching %sassc%. && EXIT /B 1
 %generate_command% || ECHO ERROR while launching `%generate_command%`. Do you have php.exe in your PATH? && EXIT /B 1
