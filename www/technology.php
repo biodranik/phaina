180e072a6ad3937c6b1ddc061994848d99086c2a
Aliaksei Maniuk <aliaksei.maniuk@gmail.com>
2017-04-02 20:14:21 -0400

    generate tech page from doc

--- www/technology.php
+++ www/technology.php
@@ -2,7 +2,6 @@
 require_once(dirname(__FILE__).'/../config.php');
 HTML_HEAD([
     'link' => 'technology',
-//    'menu' => 'menuTechnologyPage',
     'title' => 'titleTechnologyPage']);
 ?>
 
@@ -11,10 +10,14 @@ HTML_HEAD([
 <?php HTML_HEADER('technology.php'); ?>
 
 <main role="main">
-TODO: technology.php
+  <section class="section technology">
+    <div class="technology-content">
+      <h1><?= T('titleTechnologyPage') ?></h1>
+      <?php require_once('technology-content.php')?>
+    </div>
+  </section>
 </main>
 
 <?php HTML_FOOTER(); ?>
-
 </body>
 </html>
