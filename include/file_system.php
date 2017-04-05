6c0752554bd205c0b47ef6d94f5f6212b2c474b8
Aliaksei Maniuk <aliaksei.maniuk@gmail.com>
2017-04-04 22:19:21 -0400

    style fixes.

--- include/file_system.php
+++ include/file_system.php
@@ -10,20 +10,15 @@ function FullPathTo($outDir, $objectName) {
 
 function DirectoryCopy($src, $dst) { 
   $dir = opendir($src); 
-
-  if (!file_exists($dst) && !is_dir($dst)) {
+  if (!file_exists($dst) && !is_dir($dst))
     @mkdir($dst);        
-  }
 
-  while(false !== ($file = readdir($dir))) { 
-    if (($file != '.') && ($file != '..')) { 
-      if (is_dir($src . DIRECTORY_SEPARATOR . $file)) { 
+  while(($file = readdir($dir)) !== false) { 
+    if (($file != '.') && ($file != '..'))
+      if (is_dir($src . DIRECTORY_SEPARATOR . $file))
         DirectoryCopy($src . DIRECTORY_SEPARATOR . $file, $dst . DIRECTORY_SEPARATOR . $file); 
-      } 
-      else { 
+      else
         copy($src . DIRECTORY_SEPARATOR . $file, $dst . DIRECTORY_SEPARATOR . $file); 
-      } 
-    } 
   }
 
   closedir($dir); 
