891f11bf257fb0c038ac2e9a729aa56f26652cbd
Aliaksei Maniuk <aliaksei.maniuk@gmail.com>
2017-04-21 22:27:05 -0400

    Reworked faq

--- www/faq.php
+++ www/faq.php
@@ -4,27 +4,6 @@ HTML_HEAD([
     'title' => 'titleFaqPage',
     'description' => 'metaDescriptionFaqPage',
     'keywords' => 'metaKeywordsFaqPage']);
-
-$sections =[[
-  'sectionName' => T("generalQuestionsTitle"),
-  'anchor' => 'general-questions',
-  'questions' => [[
-    'question' => T("general_q1_question"),
-    'anchor' => T('general_q1_anchor'),
-    'detailedAnswer' => T("general_q1_answer")],
-  [
-    'question' => T("general_q2_question"),
-    'anchor' => T('general_q2_anchor'),
-    'detailedAnswer' => T("general_q2_answer")], 
-  [
-    'question' => T("general_q3_question"),
-    'anchor' => T('general_q3_anchor'),
-    'detailedAnswer' => T("general_q3_answer")],
-  [
-    'question' => T("general_q4_question"),
-    'anchor' => T('general_q4_anchor'),
-    'detailedAnswer' => T("general_q4_answer")]]]
-];
 ?>
 
 <body>
@@ -35,39 +14,10 @@ $sections =[[
   <section class="section">
     <h1><?= T("faqTitle") ?></h1>
     <p class="preface"><?= T("faqPreface") ?></p>
-    <?php foreach ($sections as $s) : ?>
-    <div class="faq-summary-section">
-      <h2 class="faq-summary-section__h2">
-        <a href="#<?= $s['anchor'] ?>">
-          <?= $s['sectionName'] ?>
-        </a>
-      </h2>
-      <ul class="faq-summary-section__list">
-      <?php foreach ($s['questions'] as $q) : ?>
-        <li class="faq-summary-section__question">
-          <a href="#<?= $q['anchor'] ?>">
-            <?= $q['question'] ?>
-          </a>
-        </li>
-      <?php endforeach; ?>
-      </ul>
+    <div class="faq__content">
+      <?php IncludeContent('faq') ?>
     </div>
-    <?php endforeach; ?>
   </section>
-
-  <section class="section">
-    <?php foreach ($sections as $s) : ?>
-    <h2 class="faq-main__h2" id="<?= $s['anchor'] ?>"><?= $s['sectionName'] ?></h2>
-    <div class="faq-main__block">
-      <?php foreach ($s['questions'] as $q) : ?>
-      <div class="faq-main__answer">
-        <h3 id="<?= $q['anchor'] ?>"><?= $q['question'] ?></h3>
-        <p><?= $q['detailedAnswer'] ?></p>
-      </div>
-      <?php endforeach; ?>
-    </div>
-    <?php endforeach; ?>
-  <section>
 </main>
 
 <?php HTML_FOOTER(); ?>
