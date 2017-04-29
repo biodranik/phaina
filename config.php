5b5fefccaf76fced1aa91bbcf9add6fed330d656
Alexander Zolotarev <me@alex.bio>
2017-04-28 20:50:40 -0700

    Removed ini settings which are correctly set by default.

--- config.php
+++ config.php
@@ -1,9 +1,7 @@
 <?php
-/* Must be included by inserting <?php require_once("../config.php"); ?> at the top of every page in www directory. */
-// Global PHP setting. We love UTF-8.
-ini_set('default_charset', 'UTF-8');
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', 1);
+/* Must be included by inserting <?php require_once("../config.php"); ?>
+   at the top of every page in www directory.
+*/
 
 // Production base url, also used for static pages generation.
 // Please use BaseURL() function instead of direct access, it makes development easier.
