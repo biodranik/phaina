21de08ee6f4e5480dbc0210ad0a5bb7591aad828
Alexander Zolotarev <me@alex.bio>
2017-05-04 19:51:10 -0700

    Updated technology page.

--- config.php
+++ config.php
@@ -10,7 +10,7 @@ define('LANG', 'ru');
 // Please use BaseURL() function instead of direct access, it makes development easier.
 // Url for current active language will be treated as base url.
 define('LANG_SITES', [
-  'en' => 'https://www.vibrobox.com/',
+//  'en' => 'https://www.vibrobox.com/',
   'ru' => 'https://deathbaba.github.io/phaina/']);
 define('DEMO_URL', 'https://www.demo.vibrobox.com/demo');
 define('GOOGLE_ANALYTICS_ID', 'UA-79782596-1');
