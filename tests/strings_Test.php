7a5a27842985c226a827d07dc229c7ec5530411c
Alexander Zolotarev <me@alex.bio>
2017-05-21 04:31:43 +0000

    Added StrposN function.

--- tests/strings_Test.php
+++ tests/strings_Test.php
@@ -37,5 +37,23 @@ public function testReplacePatternSmoke() {
   $this->assertEquals(self::SRC_AND_HREF_result, $html);
 }
 
+const STRPOSN = [
+  'a_b_c_d_e' => [1 /* number of _ */, 1 /* position of the found underscore */],
+  'a_b_c_d_e' => [0, 1],
+  'a_b_c_d_e' => [-2, 1],
+  'a_b_c_d_e' => [2, 3],
+  'a_b_c_d_e' => [4, 7],
+  'a_b_c_d_e' => [5, false],
+  'aasdzxcqwe' => [1, false],
+  'aasdzxcqwe' => [2, false],
+  '___' => [1, 0],
+  'родная_мова_самая_прыгожая' => [2, 11],
+];
+
+public function testStrPosN() {
+  foreach (self::STRPOSN as $str => $nAndRes)
+    $this->assertEquals($nAndRes[1], StrPosN($str, '_', $nAndRes[0]));
+}
+
 }
 ?>
